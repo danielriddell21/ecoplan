ec6aaba02a92c793c0a990ad7b743390297034f6
Dan <github.qwixl@simplelogin.co>
2026-04-18 12:53:18 +0100

    another potential fix for cloudflare tunnel

--- deploy/terraform/cloudflare.tf
+++ deploy/terraform/cloudflare.tf
@@ -15,10 +15,9 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "ecoplan" {
   config {
     ingress_rule {
       hostname = "api.${var.domain}"
-      service  = "http://ecoscan-service:50051"
+      service  = "h2c://ecoscan-service:50051"
 
       origin_request {
-        http2_origin             = true
         disable_chunked_encoding = true
       }
     }
